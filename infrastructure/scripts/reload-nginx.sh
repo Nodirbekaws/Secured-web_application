20e595630429d0919ef06492d9548451c25076ee
Nodirbek <NODIRBEKCTO@gmail.com>
2026-09-21 00:39:35 +0500

    Remove extra stuffs and adding Github Actions

--- infrastructure/scripts/reload-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Test nginx configuration before reloading
-if nginx -t; then
-    systemctl reload nginx
-else
-    echo "nginx configuration test failed. Not reloading."
-    exit 1
-fi
